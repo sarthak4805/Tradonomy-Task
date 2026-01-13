f376aea00f0330ca06ff92a39a19842f3303257d
Sarthak Shirke <sarthakshirke8@gmail.com>
2026-01-13 13:06:50 +0530

    filters added on women section

--- pages/female.js
+++ pages/female.js
@@ -1,13 +1,45 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import {client} from '../lib/client'
 import { AllProducts } from '../components'
 
+const PRODUCT_FILTERS = ['All', 'Dress', 'Sweater', 'Pants', 'Jackets']
+
 const female = ({AllFemaleProducts}) => {
+    const [activeFilter, setActiveFilter] = useState('All')
+
+    const filteredProducts = useMemo(() => {
+        if (!AllFemaleProducts) return []
+        if (activeFilter === 'All') return AllFemaleProducts
+
+        return AllFemaleProducts.filter(
+            (prod) => prod?.tags?.toLowerCase() === activeFilter.toLowerCase()
+        )
+    }, [AllFemaleProducts, activeFilter])
+
     return (
-        <div className='Allproducts-container'>
-            {AllFemaleProducts?.map(prod => (
-                <AllProducts key={prod._id} allproducts={prod} />
-            ))}
+        <div>
+            <div className='Allproducts-filter'>
+                {PRODUCT_FILTERS.map((filter) => (
+                    <button
+                        key={filter}
+                        type='button'
+                        className={filter === activeFilter ? 'active' : ''}
+                        onClick={() => setActiveFilter(filter)}
+                    >
+                        {filter}
+                    </button>
+                ))}
+            </div>
+
+            <div className='Allproducts-container'>
+                {filteredProducts?.map(prod => (
+                    <AllProducts key={prod._id} allproducts={prod} />
+                ))}
+            </div>
+
+            {!filteredProducts?.length && (
+                <p className='Allproducts-empty'>No products match this filter.</p>
+            )}
         </div>
       )
 }
